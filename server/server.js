bd59c9ba43572c8a4296dfc9c376edff5990071a
lucasmrl <nutnewbr@gmail.com>
2021-01-07 23:35:11 -0700

    Imported 'client' to 'server' folder

--- server/server.js
+++ server/server.js
@@ -2,8 +2,16 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
+const path = require("path");
 
 const PORT = process.env.PORT || 8080;
+
+// app.use(express.static(path.join(__dirname, 'client/build')));
+
+//Handling routes not defined
+// app.get('/*', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+// });
 
 let usersConnected = new Map();
 
